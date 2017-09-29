4af171618f6ad29788db836dbb063d7c0e1998d5
arvind <arvind@madmorning.com>
2017-09-29 17:12:47 +0530

    java files updated

--- test.java
+++ test.java
@@ -1,9 +1,9 @@
 public class test {
-Public static void main (String[] args)
+public static void main (String[] args)
 {
+int i;
 for(i=0;i<5;i++){
 System.out.println(" Hi test succesfull " +i);
 }
-
 }
 }
